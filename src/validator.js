8fd1208371d2e20ca37cde17ae26d4e2a49c0d2b
Pauross9 <paularosales92@gmail.com>
2022-05-15 01:01:05 -0400

    Avances en primera y segunda vista y estilos

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
+//objeto validator que tiene los métodos isValid y Maskify
 const validator = {
   // ...
 };
 
+//ciclo for para ver las posiciones pares
+
+//variable para sumar las posiciones pares e impares
+//acumulator es el acumulador y el curr es el valor actual que se va a sumar
 export default validator;
