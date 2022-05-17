3b6179e6264cb977a4b9c19dd5abf052c553a276
Pauross9 <paularosales92@gmail.com>
2022-05-16 22:19:51 -0400

    funcionalidad cambio de vistas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import validator from './validator.js';
+// import validator from './validator.js';
 
-console.log(validator);
+//Función para cambiar vistas al presionar btn comprar
+console.log("hola como estás?")
+//function bienvenida () {
+ document.getElementById('secondView').style.display = 'none';
+ document.getElementById('shoppingCart').style.display = 'block';
+// }
+console.log(document.getElementById('shoppingCart'))
+
+function validacion (){
+    document.getElementById('secondView').style.display = 'block';
+    document.getElementById('shoppingCart').style.display = 'none';
+}
+
+let arrayButtons = document.querySelectorAll('.btnComprar')
+arrayButtons.forEach(element => {
+   element.addEventListener('click', (event)=>{
+       console.log('e: ', event.target);
+       validacion()
+   })
+});
+
+//función para almacenar datos de los inputs
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,7 @@
-//objeto validator que tiene los métodos isValid y Maskify
-const validator = {
-  // ...
-};
-
+// const validator = {
+  
 //ciclo for para ver las posiciones pares
 
 //variable para sumar las posiciones pares e impares
 //acumulator es el acumulador y el curr es el valor actual que se va a sumar
-export default validator;
+// export default validator;
