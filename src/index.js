b1de98677ccf93a94551b3f0a68f925c602e6f9b
Pauross9 <paularosales92@gmail.com>
2022-05-31 14:26:06 -0400

    últimas actualizaciones

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
-// import validator from './validator.js';
+import validator from "./validator.js";
 
 //Función para cambiar vistas al presionar btn comprar
-console.log("hola como estás?")
-//function bienvenida () {
- document.getElementById('secondView').style.display = 'none';
- document.getElementById('shoppingCart').style.display = 'block';
-// }
-console.log(document.getElementById('shoppingCart'))
+//el documento entregame el elemento con el id 'secondView' 
 
-function validacion (){
-    document.getElementById('secondView').style.display = 'block';
-    document.getElementById('shoppingCart').style.display = 'none';
+document.getElementById("secondView").style.display = "none";
+document.getElementById("shoppingCart").style.display = "block";
+// }
+// console.log(document.getElementById('shoppingCart'))
+
+function validacion() {
+  document.getElementById("secondView").style.display = "block";
+  document.getElementById("shoppingCart").style.display = "none";
 }
 
-let arrayButtons = document.querySelectorAll('.btnComprar')
-arrayButtons.forEach(element => {
-   element.addEventListener('click', (event)=>{
-       console.log('e: ', event.target);
-       validacion()
-   })
+//variable para que al hacer 'click' se cambie la vista
+let arrayButtons = document.querySelectorAll(".btnComprar");
+arrayButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    //    console.log('e: ', event.target);
+    validacion();
+  });
 });
 
-//función para almacenar datos de los inputs
+//Almacenar datos de los inputs
+const btnValidator = document.getElementById("btnVal");
 
-
+//Se llama a las funciones que validan los datos
+btnValidator.addEventListener("click", function () {
+  let cardNumbers = document.getElementById("cardNumber").value;
+//Si cardNumbers es distinta de "" = true/Tarjeta válida
+//De lo contrario arroja "Tarjeta inválida"
+  if (cardNumbers !== "") {
+    let validators = validator.isValid(cardNumbers);
+    if (validators == true) {
+      //documento entregame el elemento con el id "result". El innerHTML de este elemento es "tarjeta válida"
+      document.getElementById("result").innerHTML = "Tarjeta válida";
+    } else {
+      document.getElementById("result").innerHTML = "Tarjeta inválida";
+    }
+  } else {
+    alert("Ingresa el número de tu tarjeta por favor");
+  }
+  //Función maskify
+  document.getElementById("cardNumber").value = validator.maskify(cardNumbers);
+  // console.log(validator.maskify(cardNumbers))
+  // console.log('cardNumbers', cardNumbers)
+});
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,47 @@
-// const validator = {
-  
-//ciclo for para ver las posiciones pares
+//Se declara validator como un objeto, una llave de ese objeto es isValid la otra es maskify.
+const validator = {
+  isValid: function (creditCardNumber) {
+    const splitNumbers = creditCardNumber.split("");
+    const reverseNumbers = splitNumbers.reverse("");
+    console.log(creditCardNumber);
+    console.log(splitNumbers);
+    console.log(reverseNumbers);
+   
 
-//variable para sumar las posiciones pares e impares
-//acumulator es el acumulador y el curr es el valor actual que se va a sumar
-// export default validator;
+    //   For para ver las posisciones pares
+    //un ciclo for para definir las posiciones y el if para hacer el algoritmo de Luhn
+    for (let i = 0; i < reverseNumbers.length; i++) {
+      if (i % 2 !== 0) {
+        reverseNumbers[i] = reverseNumbers[i] * 2;
+        if (reverseNumbers[i] > 9) {
+          reverseNumbers[i] = reverseNumbers[i] - 9;
+        }
+      }
+    }
+
+    // variable para sumar las posiciones pares e impares
+    const reducer = reverseNumbers.reduce(
+      (accumulator, curr) => accumulator + +curr,
+      0
+    );
+
+    let reducerValue = reducer;
+
+    if (reducerValue % 10 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  //función maskify para enmascarar números ingresados en el input 
+  maskify: function (creditCardNumber) {
+    console.log("ver:", creditCardNumber);
+    return creditCardNumber
+      .slice(0, [-4])
+      .replace(/[0-9/a-z]/g, "#")
+      .concat(creditCardNumber.slice(-4, creditCardNumber.length));
+  },
+};
+
+export default validator;
